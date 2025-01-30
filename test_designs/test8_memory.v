b619f0226a4c3dd94ca5369e01c3911e81a06363
Benjamin Correia <13207243+ben-j-c@users.noreply.github.com>
2025-01-30 00:02:47 -0500

    Add properly formed memory test. Pending memory handling.

--- test_designs/test8_memory.v
+++ test_designs/test8_memory.v
@@ -3,7 +3,7 @@ module top(
 	output reg [31:0] signal_2
 	);
 
-	reg [31:0] memory [255:0];
+	(* blackbox *) reg [31:0] memory [255:0];
 	initial $readmemh("test8_memory_hex.txt", memory);
 	always @(*) begin
 		signal_2 = memory[signal_1[7:0]];
