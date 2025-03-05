f8954e04862e58480676846e60bd0a142d473f98
Benjamin Correia <13207243+ben-j-c@users.noreply.github.com>
2025-03-04 21:30:59 -0500

    Placement API wip

--- src/physical_planning.rs
+++ src/physical_planning.rs
@@ -344,42 +344,34 @@ pub(crate) fn overflowing_cells_swap_local_method(
 	max_density: i32,
 ) {
 	let mut swap_cells = vec![];
-	for (idx, (cx, cy)) in new_assignments.iter().enumerate() {
-		if new_block_counts[*cx][*cy] > 1 {
+	for (idx, assignment) in new.assignments.iter().enumerate() {
+		if new.density(*assignment) > 1 {
 			swap_cells.push(idx);
 		}
 	}
 
 	for cell in &swap_cells {
-		let (cx, cy) = new_assignments[*cell];
+		let assignment = new.assignment(*cell);
 		loop {
 			let pick_x: i32 = rng.random_range(0..=1) * 2;
 			let pick_y: i32 = rng.random_range(-1..=1);
-			let want_x = cx as isize + pick_x as isize;
-			let want_y = cy as isize + pick_y as isize;
-			let want_assignment = (want_x, want_y);
-			if (cx as isize, cy as isize) == want_assignment
-				|| want_assignment.0 < 0
-				|| want_assignment.0 >= side_length as isize
-				|| want_assignment.1 < 0
-				|| want_assignment.1 >= side_length as isize
+			let want_x = assignment.0 as isize + pick_x as isize;
+			let want_y = assignment.1 as isize + pick_y as isize;
+			let want_assignment = (want_x as usize, want_y as usize);
+			if assignment == want_assignment
+				|| want_assignment.0 >= side_length
+				|| want_assignment.1 >= side_length
 			{
 				continue;
 			}
-			let want_assignment = (want_x as usize, want_y as usize);
 			let mut did_swap = false;
-			for (cell2, cxy2) in new_assignments.iter().enumerate() {
-				if *cxy2 != want_assignment {
-					continue;
-				}
-				did_swap = true;
-				new_assignments.swap(*cell, cell2);
-				break;
-			}
-			if !did_swap {
-				new_assignments[*cell] = want_assignment;
-				new_block_counts[want_assignment.0][want_assignment.1] += 1;
-				new_block_counts[cx][cy] -= 1;
+
+			let candidates = new.id_at(want_assignment).iter().collect_vec();
+			if candidates.is_empty() {
+				new.mov(*cell, want_assignment);
+			} else {
+				let candidate = candidates[rng.random_range(0..candidates.len())];
+				new.swap(*candidate, *cell);
 			}
 			break;
 		}
@@ -388,29 +380,17 @@ pub(crate) fn overflowing_cells_swap_local_method(
 
 pub(crate) fn simulated_spring_method(
 	rng: &mut StdRng,
-	_assignments: &Vec<(usize, usize)>,
-	_block_counts: &Vec<Vec<i32>>,
-	new_assignments: &mut Vec<(usize, usize)>,
-	new_block_counts: &mut Vec<Vec<i32>>,
+	curr: &Placement,
+	new: &mut Placement,
 	connections_per_node: &Vec<Vec<usize>>,
 	side_length: usize,
 	max_density: i32,
 ) {
-	let num_cells = new_assignments.len();
+	let num_cells = new.num_cells();
 	let iters = rng.random_range(1..100);
 	for _ in 0..iters {
-		let mut pos_map: HashMap<(isize, isize), BTreeSet<usize>> = HashMap::new();
-		for (idx, cxcy) in new_assignments.iter().enumerate() {
-			match pos_map.entry((cxcy.0 as isize, cxcy.1 as isize)) {
-				std::collections::hash_map::Entry::Occupied(mut occupied_entry) => {
-					occupied_entry.get_mut().insert(idx);
-				}
-				std::collections::hash_map::Entry::Vacant(vacant_entry) => {
-					vacant_entry.insert(BTreeSet::from_iter(vec![idx]));
-				}
-			};
-		}
-		let mut force = new_assignments
+		let mut force = new
+			.assignments
 			.iter()
 			.enumerate()
 			.map(|(idx, (x0, y0))| {
@@ -419,7 +399,7 @@ pub(crate) fn simulated_spring_method(
 					connections_per_node[idx]
 						.iter()
 						.map(|idx2| {
-							let (x1, y1) = new_assignments[*idx2];
+							let (x1, y1) = new.assignment(*idx2);
 							let dx = x1 as isize - *x0 as isize;
 							let dy = y1 as isize - *y0 as isize;
 							(dx * dx * dx.signum(), dy * dy * dy.signum())
@@ -429,7 +409,7 @@ pub(crate) fn simulated_spring_method(
 				)
 			})
 			.collect_vec();
-		for (cell1, (x, y)) in new_assignments.iter().enumerate() {
+		for (cell1, (x, y)) in new.assignments.iter().enumerate() {
 			let offsets = vec![-2, 0, 2]
 				.into_iter()
 				.cartesian_product(vec![-1, 0, 1])
@@ -438,11 +418,11 @@ pub(crate) fn simulated_spring_method(
 				if dx == 0 && dy == 0 {
 					continue;
 				}
-				for cell2 in pos_map
-					.entry((*x as isize + dx, *y as isize + dy))
-					.or_default()
-					.iter()
-				{
+				let check_pos = ((*x as isize + dx) as usize, (*y as isize + dy) as usize);
+				if check_pos.0 >= side_length || check_pos.1 >= side_length {
+					continue;
+				}
+				for cell2 in new.id_at(check_pos) {
 					if connections_per_node[cell1].contains(cell2) {
 						continue;
 					}
@@ -453,6 +433,7 @@ pub(crate) fn simulated_spring_method(
 				}
 			}
 		}
+		let force_map = force.clone();
 		force.sort_by(|(_, fa), (_, fb)| {
 			(fa.0 * fa.0 + fa.1 * fa.1)
 				.cmp(&(fb.0 * fb.0 + fb.1 * fb.1))
@@ -464,96 +445,73 @@ pub(crate) fn simulated_spring_method(
 			if *fx == 0 && *fy == 0 {
 				continue;
 			}
-			let offset = if 2 * fx.abs() < fy.abs() {
-				match *fy > 0 {
-					true => (0, 1),
-					false => (0, -1),
+			let assignment1 = new.assignment(*idx1);
+			let candidates = if *fx > 0 {
+				if *fy > 0 {
+					new.id_in_range_exclude_a1(
+						assignment1,
+						(assignment1.0 as isize + 6, assignment1.1 as isize + 3),
+					)
+				} else if *fy == 0 {
+					new.id_in_range_exclude_a1(
+						assignment1,
+						(assignment1.0 as isize + 6, assignment1.1 as isize),
+					)
+				} else {
+					new.id_in_range_exclude_a1(
+						assignment1,
+						(assignment1.0 as isize + 6, assignment1.1 as isize - 3),
+					)
 				}
-			} else if 2 * fy.abs() < fx.abs() {
-				match *fx > 0 {
-					true => (2, 0),
-					false => (-2, 0),
+			} else if *fx == 0 {
+				if *fy > 0 {
+					new.id_in_range_exclude_a1(
+						assignment1,
+						(assignment1.0 as isize, assignment1.1 as isize + 3),
+					)
+				} else if *fy == 0 {
+					new.id_in_range_exclude_a1(
+						assignment1,
+						(assignment1.0 as isize, assignment1.1 as isize),
+					)
+				} else {
+					new.id_in_range_exclude_a1(
+						assignment1,
+						(assignment1.0 as isize, assignment1.1 as isize - 3),
+					)
 				}
 			} else {
-				match (*fx > 0, *fy > 0) {
-					(true, true) => (2, 1),
-					(true, false) => (2, -1),
-					(false, true) => (-2, 1),
-					(false, false) => (-2, -1),
+				if *fy > 0 {
+					new.id_in_range_exclude_a1(
+						assignment1,
+						(assignment1.0 as isize - 6, assignment1.1 as isize + 3),
+					)
+				} else if *fy == 0 {
+					new.id_in_range_exclude_a1(
+						assignment1,
+						(assignment1.0 as isize - 6, assignment1.1 as isize),
+					)
+				} else {
+					new.id_in_range_exclude_a1(
+						assignment1,
+						(assignment1.0 as isize - 6, assignment1.1 as isize - 3),
+					)
 				}
 			};
-			let (cx, cy) = new_assignments[*idx1];
-			let want_assignment = (
-				(cx as isize + offset.0) as usize,
-				(cy as isize + offset.1) as usize,
-			);
-			let want_assignmentisize = ((cx as isize + offset.0), (cy as isize + offset.1));
-			let mut got_want = false;
-			let mut kicked_out = usize::MAX;
-			match pos_map.entry(want_assignmentisize) {
-				std::collections::hash_map::Entry::Vacant(vacant_entry) => {
-					vacant_entry.insert(BTreeSet::new());
-				}
-				_ => {}
-			};
-			if want_assignmentisize.0 < 0
-				|| want_assignmentisize.0 >= side_length as isize
-				|| want_assignmentisize.1 < 0
-				|| want_assignmentisize.1 >= side_length as isize
-			{
+			if candidates.is_empty() {
 				continue;
 			}
-			if new_block_counts[want_assignment.0][want_assignment.1] < (max_density - 1).max(1) {
-				new_assignments[*idx1] = want_assignment;
-				new_block_counts[cx][cy] -= 1;
-				new_block_counts[want_assignment.0][want_assignment.1] += 1;
-				got_want = true;
-			} else {
-				match pos_map.get_mut(&want_assignmentisize) {
-					Some(idx2s) => {
-						let mut min_force = isize::MAX;
-						let f_incoming = f1.0 * f1.0 + f1.1 * f1.1;
-						for idx2 in idx2s.iter() {
-							let (_, f2) = force[*idx2];
-							let f_current = f2.0 * f2.0 + f2.1 * f2.1;
-							if f_incoming > f_current && f_current < min_force {
-								new_assignments[*idx1] = want_assignment;
-								got_want = true;
-								kicked_out = *idx2;
-								min_force = f_current;
-							}
-						}
-						if (got_want) {
-							new_assignments[kicked_out] = (cx, cy);
-						}
-					}
-					None => {
-						new_assignments[*idx1] = want_assignment;
-						new_block_counts[cx][cy] -= 1;
-						new_block_counts[want_assignment.0][want_assignment.1] += 1;
-						got_want = true;
-					}
-				}
-			}
-			if got_want {
-				pos_map
-					.get_mut(&want_assignmentisize)
-					.unwrap()
-					.insert(*idx1);
-				pos_map
-					.get_mut(&(cx as isize, cy as isize))
-					.unwrap()
-					.remove(&idx1);
-				if kicked_out != usize::MAX {
-					pos_map
-						.get_mut(&want_assignmentisize)
-						.unwrap()
-						.remove(&kicked_out);
-					pos_map
-						.get_mut(&(cx as isize, cy as isize))
-						.unwrap()
-						.insert(kicked_out);
-				}
+			let candidate = candidates
+				.iter()
+				.min_by(|a, b| {
+					let fa = force_map[**a].1;
+					let fb = force_map[**b].1;
+					(fa.0 * fa.0 + fa.1 * fa.1).cmp(&(fb.0 * fb.0 + fb.1 * fb.1))
+				})
+				.unwrap();
+			let (fx2, fy2) = force_map[*candidate].1;
+			if fx2 * fx2 + fy2 * fy2 < fx * fx + fy * fy {
+				new.swap(*idx1, *candidate);
 			}
 		}
 	}
@@ -561,15 +519,13 @@ pub(crate) fn simulated_spring_method(
 
 pub(crate) fn slide_puzzle_method_on_violations(
 	rng: &mut StdRng,
-	assignments: &Vec<(usize, usize)>,
-	_block_counts: &Vec<Vec<i32>>,
-	new_assignments: &mut Vec<(usize, usize)>,
-	new_block_counts: &mut Vec<Vec<i32>>,
+	curr: &Placement,
+	new: &mut Placement,
 	connections_per_node: &Vec<Vec<usize>>,
 	side_length: usize,
 	max_density: i32,
 ) {
-	let num_cells = assignments.len();
+	let num_cells = new.num_cells();
 	let iters = 1;
 	for _ in 0..iters {
 		let mut interesting_cells = BTreeSet::new();
@@ -578,8 +534,8 @@ pub(crate) fn slide_puzzle_method_on_violations(
 				if *j < i {
 					continue;
 				}
-				let (x_i, y_i) = new_assignments[i];
-				let (x_j, y_j) = new_assignments[*j];
+				let (x_i, y_i) = new.assignment(i);
+				let (x_j, y_j) = new.assignment(*j);
 				let dx = (x_i as isize - x_j as isize).abs() as f64;
 				let dy = (y_i as isize - y_j as isize).abs() as f64;
 				let r2distance = dx.powi(2) + dy.powi(2);
@@ -593,7 +549,7 @@ pub(crate) fn slide_puzzle_method_on_violations(
 			let want_assignment = connections_per_node[picked_cell]
 				.iter()
 				.map(|idx2| {
-					let (x1, y1) = new_assignments[*idx2];
+					let (x1, y1) = new.assignment(*idx2);
 					(x1, y1)
 				})
 				.reduce(|(x0, y0), (x1, y1)| (x0 + x1, y0 + y1))
@@ -609,112 +565,19 @@ pub(crate) fn slide_puzzle_method_on_violations(
 				continue;
 			};
 
-			if want_assignment == new_assignments[picked_cell] {
+			if want_assignment == new.assignment(picked_cell) {
 				continue;
 			}
-
-			if rng.random_bool(0.5) {
-				let line_y = want_assignment.1;
-				if rng.random_bool(0.5) {
-					let mut ripup_cells = vec![];
-					for cell in 0..num_cells {
-						let (cx, cy) = new_assignments[cell];
-						if cy == 0 {
-							continue;
-						}
-						if cy <= line_y && cx == want_assignment.0 {
-							ripup_cells.push(cell);
-							new_block_counts[cx][cy] -= 1;
-						}
-					}
-					ripup_cells.sort_by(|a, b| new_assignments[*a].1.cmp(&new_assignments[*b].1));
-					for cell in ripup_cells {
-						let (cx, cy) = new_assignments[cell];
-						if new_block_counts[cx][cy - 1] < max_density {
-							new_block_counts[cx][cy - 1] += 1;
-							new_assignments[cell] = (cx, cy - 1);
-						} else {
-							new_block_counts[cx][cy] += 1;
-						}
-					}
-				} else {
-					let mut ripup_cells = vec![];
-					for cell in 0..num_cells {
-						let (cx, cy) = new_assignments[cell];
-						if cy == side_length - 1 {
-							continue;
-						}
-						if cy >= line_y && cx == want_assignment.0 {
-							ripup_cells.push(cell);
-							new_block_counts[cx][cy] -= 1;
-						}
-					}
-					ripup_cells.sort_by(|a, b| new_assignments[*b].1.cmp(&new_assignments[*a].1));
-					for cell in ripup_cells {
-						let (cx, cy) = new_assignments[cell];
-						if new_block_counts[cx][cy + 1] < max_density {
-							new_block_counts[cx][cy + 1] += 1;
-							new_assignments[cell] = (cx, cy + 1);
-						} else {
-							new_block_counts[cx][cy] += 1;
-						}
-					}
+			for _ in 0..8 {
+				new.slide(
+					want_assignment,
+					max_density,
+					rng.random_bool(0.5),
+					rng.random_bool(0.5),
+				);
+				if new.density(want_assignment) < max_density {
+					new.mov(picked_cell, want_assignment);
 				}
-			} else {
-				let line_x = want_assignment.0;
-				if rng.random_bool(0.5) {
-					let mut ripup_cells = Vec::new();
-					for cell in 0..num_cells {
-						let (cx, cy) = new_assignments[cell];
-						if cx == 0 {
-							continue;
-						}
-						if cx >= line_x && cy == want_assignment.1 {
-							ripup_cells.push(cell);
-							new_block_counts[cx][cy] -= 1;
-						}
-					}
-					ripup_cells.sort_by(|a, b| new_assignments[*b].0.cmp(&new_assignments[*a].0));
-
-					for cell in ripup_cells {
-						let (cx, cy) = new_assignments[cell];
-						if new_block_counts[cx - 2][cy] < max_density {
-							new_block_counts[cx - 2][cy] += 1;
-							new_assignments[cell] = (cx - 2, cy);
-						} else {
-							new_block_counts[cx][cy] += 1;
-						}
-					}
-				} else {
-					let mut ripup_cells = Vec::new();
-					for cell in 0..num_cells {
-						let (cx, cy) = new_assignments[cell];
-						if cx == (side_length - 1) / 2 * 2 {
-							continue;
-						}
-						if cx <= line_x && cy == want_assignment.1 {
-							ripup_cells.push(cell);
-							new_block_counts[cx][cy] -= 1;
-						}
-					}
-					ripup_cells.sort_by(|a, b| new_assignments[*a].0.cmp(&new_assignments[*b].0));
-
-					for cell in ripup_cells {
-						let (cx, cy) = new_assignments[cell];
-						if new_block_counts[cx + 2][cy] < max_density {
-							new_block_counts[cx + 2][cy] += 1;
-							new_assignments[cell] = (cx + 2, cy);
-						} else {
-							new_block_counts[cx][cy] += 1;
-						}
-					}
-				}
-			}
-			let curr_assignment = new_assignments[picked_cell];
-			if new_block_counts[want_assignment.0][want_assignment.1] < max_density {
-				new_block_counts[want_assignment.0][want_assignment.1] += 1;
-				new_block_counts[curr_assignment.0][curr_assignment.1] -= 1;
-				new_assignments[picked_cell] = want_assignment;
 			}
 		}
 	}
@@ -884,6 +747,34 @@ impl Placement {
 
 	pub fn num_cells(&self) -> Num {
 		self.assignments.len()
+	}
+
+	pub(crate) fn id_in_range_exclude_a1(
+		&self,
+		assignment1: (Num, Num),
+		assignment2: (isize, isize),
+	) -> HashSet<Num, BuildHasherDefault<FNV1aHasher64>> {
+		let assignment2 = (
+			assignment2
+				.0
+				.min((self.side_length as isize - 1) * 2 / 2)
+				.max(0) as usize,
+			assignment2.1.min(self.side_length as isize - 1).max(0) as usize,
+		);
+		let min_x = assignment1.0.min(assignment2.0);
+		let min_y = assignment1.1.min(assignment2.1);
+		let max_x = assignment1.0.max(assignment2.0);
+		let max_y = assignment1.1.max(assignment2.1);
+		let mut ret = hash_set();
+		for x in min_x..=max_x {
+			for y in min_y..=max_y {
+				if x >= self.side_length || y >= self.side_length {
+					continue;
+				}
+				ret.extend(self.id_at((x, y)));
+			}
+		}
+		ret
 	}
 
 	pub(crate) fn id_at(
