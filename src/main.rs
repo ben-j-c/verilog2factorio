633fb4cffbc808a70298ee0618f3e1a42f1a17e9
Ben <13207243+ben-j-c@users.noreply.github.com>
2025-04-13 22:28:36 -0400

    Remove file

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ use serializable_design::SerializableDesign;
 pub mod checked_design;
 mod connected_design;
 mod logical_design;
-pub mod logical_design_safe;
 mod mapped_design;
 mod phy;
 mod serializable_design;
