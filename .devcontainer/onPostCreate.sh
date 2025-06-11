8dc3e62db16ce508c8b1c8fc5a585cda337b7b00
Ben <13207243+ben-j-c@users.noreply.github.com>
2025-06-11 02:27:11 -0400

    Fix dev container?

---
+++ .devcontainer/onPostCreate.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+git submodule update --init --recursive
+
+if [ ! $? -eq 0 ] ; then
+	git clone --depth 1 --branch v0.52  git@github.com:YosysHQ/yosys.git
+	cd yosys
+	git submodule update --init --recursive
+	cd ..
+fi
+
+set -euo pipefail
+
+rust() {
+  cargo build --release
+}
+
+yosys() {
+  cd yosys
+  make config-gcc 
+  make -j`nproc`
+}
+
+rust >rust.log 2>&1 & pid1=$!
+yosys | tee yosys.log & pid2=$!
+
+echo "Building binaries"
+
+wait "$pid1"
+echo "V2F done"
+wait "$pid2"
+echo "Yosys done"
+
+echo "All done"
