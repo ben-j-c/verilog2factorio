a28abe45c15697945f28e332a284f3d1775d7475
Ben <13207243+ben-j-c@users.noreply.github.com>
2026-01-01 17:08:33 -0500

    Bandaid fix

--- techmap/v2f_simplify.v
+++ techmap/v2f_simplify.v
@@ -500,3 +500,29 @@ module v2f_rule_gt_signed_expand(A, B, Y);
 			assign Y = A_internal >= B_internal;
 	endgenerate
 endmodule
+
+// This shouldn need to exist! BUG IN checked_design.rs
+(* techmap_celltype = "$adffe" *)
+module v2f_rule_adffe_widen(CLK, ARST, EN, D, Q);
+	parameter WIDTH = 32;
+	parameter CLK_POLARITY = 1'b1;
+	parameter EN_POLARITY = 1'b1;
+	parameter ARST_POLARITY = 1'b1;
+	parameter ARST_VALUE = 0;
+	localparam WIDTH_EXT = 32 - WIDTH;
+
+	wire _TECHMAP_FAIL_ = WIDTH == 32;
+
+	input CLK, ARST, EN;
+	input [WIDTH-1:0] D;
+	output reg [31:0] Q;
+	wire pos_clk = CLK == CLK_POLARITY;
+	wire pos_arst = ARST == ARST_POLARITY;
+
+	always @(posedge pos_clk, posedge pos_arst) begin
+		if (pos_arst)
+			Q <= ARST_VALUE;
+		else if (EN == EN_POLARITY)
+			Q <= {{WIDTH_EXT{1'b0}}, D};
+	end
+endmodule
